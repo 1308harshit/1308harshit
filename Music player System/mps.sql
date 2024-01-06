85e00f043190fdfebce0b6fc50ab55913df4663a
Harshit Khatsuriya <139879303+1308harshit@users.noreply.github.com>
2024-01-06 21:14:34 +0530

    Update mps.sql

--- Music player System/mps.sql
+++ Music player System/mps.sql
@@ -1,32 +1,7 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Oct 08, 2023 at 10:13 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `mps`
---
-
--- --------------------------------------------------------
 
 --
 -- Table structure for table `adityagadhvisongs`
 --
-
 CREATE TABLE `adityagadhvisongs` (
   `id` int(20) NOT NULL,
   `songname` varchar(50) NOT NULL,
